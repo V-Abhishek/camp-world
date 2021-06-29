009e9a9f9a42bb7a1087de19c8ae0ca145222fbb
V-Abhishek <abhishekv2603@gmail.com>
2021-06-29 18:01:35 -0400

    storage change

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
-const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+const secret = process.env.SECRET || 'LocalRunSecret!';
 
 const store = new MongoDBStore({
     url: dbUrl,
